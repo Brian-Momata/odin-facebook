1fb3391f3f3b022fe9c5d6fe01b819c2bb83d2f6
Brian-Momata <brianmomata@gmail.com>
2023-05-23 10:38:06 +0300

    Add routes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,25 +1,6 @@
 class LikesController < ApplicationController
-  before_action :set_like, only: %i[ show edit update destroy ]
+  before_action :set_like, only: [:destroy]
 
-  # GET /likes or /likes.json
-  def index
-    @likes = Like.all
-  end
-
-  # GET /likes/1 or /likes/1.json
-  def show
-  end
-
-  # GET /likes/new
-  def new
-    @like = Like.new
-  end
-
-  # GET /likes/1/edit
-  def edit
-  end
-
-  # POST /likes or /likes.json
   def create
     @like = Like.new(like_params)
 
@@ -29,19 +10,6 @@ class LikesController < ApplicationController
         format.json { render :show, status: :created, location: @like }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @like.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /likes/1 or /likes/1.json
-  def update
-    respond_to do |format|
-      if @like.update(like_params)
-        format.html { redirect_to like_url(@like), notice: "Like was successfully updated." }
-        format.json { render :show, status: :ok, location: @like }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @like.errors, status: :unprocessable_entity }
       end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :likes
-  resources :posts
+  root "posts#index"
+
   devise_for :users
+
+  resources :posts do
+    resources :comments
+    resources :likes, only: [:create, :destroy]
+  end
+
+  resources :users, except: [:create, :destroy, :edit, :new, :update] do
+    member do
+      post 'follow', to: 'follows#create'  # Create a follow relationship
+      delete 'unfollow', to: 'follows#destroy'  # Remove a follow relationship
+    end
+
+    resources :posts, only: [:create, :new, :edit, :update, :destroy]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
