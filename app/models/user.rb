fc1e3358d014b998ea495e81f320bf0cad9183bb
Brian-Momata <brianmomata@gmail.com>
2023-05-23 09:22:47 +0300

    Configure db with postgresql

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts
+  has_many :likes
+  has_many :comments
+
+  has_many :follows, class_name: "follow", foreign_key: "follower_id"
+  has_many :followed_users, through: :follows, source: :followed_user
+  has_many :followers, class_name: "follow", foreign_key: "followed_user_id"
+  has_many :follower_users, through: :followers, source: :follower
 end
