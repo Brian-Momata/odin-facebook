32871c338cc78d9c502b8e8f96bcf4653a054799
Brian-Momata <brianmomata@gmail.com>
2023-05-29 10:45:48 +0300

    Add status to follows table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_22_073528) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_074309) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_22_073528) do
     t.integer "followed_user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "pending"
   end
 
   create_table "likes", force: :cascade do |t|
