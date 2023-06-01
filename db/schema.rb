cbf1a764c5eb283bfc6ce7d4b71fdce1290d53fd
Brian-Momata <brianmomata@gmail.com>
2023-06-01 10:18:22 +0300

    Add index to ensure unique combination

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_074309) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_01_071218) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_074309) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status", default: "pending"
+    t.index ["follower_id", "followed_user_id"], name: "index_follows_on_follower_id_and_followed_user_id", unique: true
   end
 
   create_table "likes", force: :cascade do |t|
