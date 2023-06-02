35c708c93855971cfb6bf6b406a2a319f7e566e2
Brian-Momata <brianmomata@gmail.com>
2023-06-02 08:25:58 +0300

    Change post/user association to author

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
-  belongs_to :user
+  belongs_to :author, class_name: "User", foreign_key: "user_id"
   
   validates :body, presence: true
   
