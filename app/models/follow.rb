85989533db26bbb594ffe0fb6a804e0e3e3b7b37
Brian-Momata <brianmomata@gmail.com>
2023-06-10 10:27:54 +0300

    Add scope to notifications

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,4 +2,6 @@ class Follow < ApplicationRecord
   belongs_to :follower, class_name: "User", foreign_key: "follower_id"
   belongs_to :followed_user, class_name: "User", foreign_key: "followed_user_id"
   has_one :notification, dependent: :destroy
+
+  scope :accepted, -> { where(:status => "Accepted")}
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,4 +2,7 @@ class Notification < ApplicationRecord
   belongs_to :sender, class_name: "User", foreign_key: "sender_id"
   belongs_to :recipient, class_name: "User", foreign_key: "recipient_id"
   belongs_to :follow
+
+  scope :pending_requests, -> { 
+    joins(:follow).where(follows: { status: "pending" }) }
 end
