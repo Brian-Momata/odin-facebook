34f03c8026bb6b238d324242dad75e78ac6b59ad
Brian-Momata <brianmomata@gmail.com>
2023-06-23 10:42:04 +0300

    Ensure user data gets destroyed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,27 @@
 class User < ApplicationRecord
   include Gravtastic
   gravtastic
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook]
 
-  has_many :posts
-  has_many :likes
-  has_many :comments
-  has_many :notifications, foreign_key: "recipient_id"
+  has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :notifications, foreign_key: "recipient_id", dependent: :destroy
 
-  has_many :follows, class_name: "Follow", foreign_key: "follower_id"
+  has_many :follows, class_name: "Follow", foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, -> { where("follows.status = ?", "Accepted") },
     through: :follows,
-    source: :followed_user
-  has_many :followers, class_name: "Follow", foreign_key: "followed_user_id"
+    source: :followed_user,
+    dependent: :destroy
+  has_many :followers, class_name: "Follow", foreign_key: "followed_user_id", dependent: :destroy
   has_many :follower_users,-> { where("follows.status = ?", "Accepted") },
     through: :followers,
-    source: :follower
+    source: :follower,
+    dependent: :destroy
 
   # finds or creates a user based on the OmniAuth authentication data
   def self.from_omniauth(auth)
