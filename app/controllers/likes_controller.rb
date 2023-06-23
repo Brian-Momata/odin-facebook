7d9a73862e4d1353d77e3ece1ec22bfb5b1152c6
Brian-Momata <brianmomata@gmail.com>
2023-06-23 11:09:13 +0300

    Allow unliking post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,4 @@
 class LikesController < ApplicationController
-  before_action :set_like, only: [:destroy]
-
-  # POST /posts/:id/likes
   def create
     @post = Post.find(params[:post_id])
     @like = @post.likes.build(post_id: @post.id, user_id: current_user.id )
@@ -14,14 +11,10 @@ class LikesController < ApplicationController
     redirect_to @post
   end
 
-  # DELETE /posts/:id/likes/1 or /likes/1.json
   def destroy
-    
-  end
-
-  private
-    
-    def set_like
-      @like = Like.find(params[:id])
-    end
+    @like = Like.find(params[:id])
+    @like.destroy
+    flash[:notice] = "You've unliked the post"
+    redirect_to @like.post
+  end  
 end
